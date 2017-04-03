02a8b23ef18b0cde5a343431071e974b50adfaba
mohammedubaid <mohammedubaidc@gmail.com>
2017-04-04 01:20:36 +0530

    removed webpack changed to react scripts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import '../style/style.css';
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
 class App extends Component {
